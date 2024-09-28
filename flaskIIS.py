a6887cf28e416f414880dc47bf95e9736b0f6e9d
Hemant kalal <78431305+hkalal@users.noreply.github.com>
2024-09-28 12:39:34 +0530

    Add files via upload

--- flaskIIS.py
+++ flaskIIS.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from data_ingestion import DataIngestion
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,7 +8,10 @@ def hello_world():
 
 @app.route('/test')
 def hello_world1():
-   return 'Hello World - test'
+   print('test')
+   obj=DataIngestion()
+   test_data=obj.initiate_data_ingestion()
+   return 'Hello World - test1'
 
 if __name__ == '__main__':
    app.run()
---
+++ predict_pipeline.py
@@ -0,0 +1,32 @@
+import sys
+import pandas as pd
+import os
+import pickle
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path= os.path.join("artifacts","model.pkl")
+            preprocessor_path= os.path.join('artifacts','proprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            pass
+
+
+    def load_object(self,file_path):
+        try:
+            with open(file_path, "rb") as file_obj:
+                return pickle.load(file_obj)
+
+        except Exception as e:
+            pass
