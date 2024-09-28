c1f86cfd17aad2bde4c41b192e3084717316bf2f
Hemant kalal <78431305+hkalal@users.noreply.github.com>
2024-09-28 11:22:02 +0530

    Update flaskIIS.py

--- flaskIIS.py
+++ flaskIIS.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello World'
 
+@app.route('/test')
+def hello_world():
+   return 'Hello World - test'
+
 if __name__ == '__main__':
    app.run()
